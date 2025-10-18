385a405d93b972ace53cd7b46da84fc52cc8c88d
Fedexyz13 <federicovyt13@gmail.com>
2025-10-18 09:25:07 -0300

    Update jadibot-resetbot.js

--- plugins/jadibot-resetbot.js
+++ plugins/jadibot-resetbot.js
@@ -2,13 +2,13 @@ let handler = async (m, { conn }) => {
     let chat = global.db.data.chats[m.chat];
 
     if (!chat || !chat.primaryBot) {
-        return m.reply('《✧》 No hay ningún bot primario establecido en este grupo.');
+        return m.reply('《 ⚽ 》 No hay ningún bot primario establecido en este grupo.');
     }
 
     console.log(`[ResetBot] Reseteando configuración para el chat: ${m.chat}`);
     chat.primaryBot = null;
 
-    await m.reply(`✐ ¡Listo! Se ha restablecido la configuración.\n> A partir de ahora, todos los bots válidos responderán nuevamente en este grupo.`);
+    await m.reply(`🌙 ¡Listo! Se ha restablecido la configuración.\n> A partir de ahora, todos los bots válidos responderán nuevamente en este grupo.`);
 }
 
 handler.customPrefix = /^(resetbot|resetprimario|botreset)$/i;
