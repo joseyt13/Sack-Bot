593fa68a498a467aef09072e61be96aef7b344ed
Fedexyz13 <federicovyt13@gmail.com>
2025-10-18 09:24:08 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,13 +1,13 @@
 import { exec } from 'child_process';
 
 let handler = async (m, { conn }) => {
-  m.reply(`🌷 Actualizando la bot...`);
+  m.reply(`⚽ Actualizando la bot...`);
 
   const comando = 'find src -type f | xargs git update-index --assume-unchanged && git pull';
 
   exec(comando, (err, stdout, stderr) => {
     if (err) {
-      conn.reply(m.chat, `🍭 Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
+      conn.reply(m.chat, `🌙 Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
       return;
     }
 
@@ -16,7 +16,7 @@ let handler = async (m, { conn }) => {
     }
 
     if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, `🌷 La bot ya está actualizada.`, m);
+      conn.reply(m.chat, `⚽ La bot ya está actualizada.`, m);
     } else {
       conn.reply(m.chat, `🍭 Actualización realizada con éxito.\n\n${stdout}`, m);
     }
