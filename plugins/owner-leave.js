afb4c9a61c44940c18b8b27a0fc001b3f9da512e
Fedexyz13 <federicovyt13@gmail.com>
2025-10-20 19:28:49 -0300

    Create owner-leave.js

---
+++ plugins/owner-leave.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn, command}) => {
+
+  if (!m.isGroup) throw '🌿 Este comando solo puede usarse en grupos.';
+
+
+  if (!global.owner.includes(m.sender)) throw '🍂 Solo el propietario del bot puede usar este comando.';
+
+  const groupMetadata = await conn.groupMetadata(m.chat);
+  const namebog = groupMetadata.subject;
+
+  await conn.sendMessage(m.chat, {
+    text: `🌿 Adiós *${botname}*, se despide....`,
+});
+
+  await conn.groupLeave(m.chat);
+};
+
+handler.command = ['salir'];
+handler.tags = ['owner'];
+handler.help = ['salir'];
+handler.rowner = true;
+
+export default handler;
