c75651f96510f18c030952dcc33e250d9361a7f9
Fedexyz13 <federicovyt13@gmail.com>
2025-10-20 11:31:53 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 # Código desarrollado por @gata_dios 
   # * Al bifurcar este repositorio, dejen los créditos a todos los creadores de cada código por favor. ♥
 #!/data/data/com.termux/files/usr/bin/bash 
-BOT_DIR="Ruby-Hoshino-Bot"  
-BOT_REPO="https://github.com/Dioneibi-rip/$BOT_DIR"
+BOT_DIR="Nagi-Bot"  
+BOT_REPO="https://github.com/Dev-fedexyz13/$BOT_DIR"
 DB_FILE="database.json"
 
 GREEN='\033[32m'
