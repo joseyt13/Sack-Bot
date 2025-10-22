f062e1930e1ea0c48740719d0cc20041081d1d5c
Fedexyz13 <federicovyt13@gmail.com>
2025-10-22 11:22:26 -0300

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -224,7 +224,7 @@ await joinChannels(sock)
 if (m?.chat) {
   const mensaje = args[0]
 ? `@${m.sender.split('@')[0]}, ya estás conectado, leyendo mensajes entrantes...`
-: `@${m.sender.split('@')[0]} 🍃 *_Bienvenid@ a la conexión de Nagi-BotV1._*`
+: `@${m.sender.split('@')[0]}`
 
   await conn.sendMessage(
     m.chat,
@@ -235,11 +235,11 @@ if (m?.chat) {
   const usuario = `@${m.sender.split('@')[0]}`
   await conn.reply(
     m.chat,
-    `${usuario}, 🌿 *_Genial, ya eres parte de la familia Sub-Bots._*`,
+    `${usuario}, 🌿 Genial, ya eres parte de la familia Sub-Bots.`,
     m,
     global.rcanal
 )
-}
+    }
     
 }}
 setInterval(async () => {
